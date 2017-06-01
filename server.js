992e81d056488f539ad7b34391a3e6b63774e618
Coffeeteer <tgrieshiemer@gmail.com>
2017-06-01 14:57:13 -0400

    2nd commit

--- server.js
+++ server.js
@@ -5,8 +5,23 @@ var express = require('express');
 
 const app = express();
 
+app.use( (request, response, next) =>{
+	console.log('In middleware 1');
+	response.write('HEADER ');
+	next();
+	console.log('Out of Middleware 1');
+});
+
+app.use((request, response, next) => {
+	console.log('In Middleware 2');
+	response.write('OTHER ')
+	next();
+	console.log('Out of Middleware 2');
+});
+
 app.get('/', (request, response) =>{
-	response.end('Hello World.')
+	response.end('Hello World.');
+	console.log('In Handler');
 });
 
 const server = new http.Server(app);
