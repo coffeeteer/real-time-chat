1459c9088fb4c998f7f4f4772c462709e003d225
Coffeeteer <tgrieshiemer@gmail.com>
2017-06-02 12:03:21 -0400

    using jade/pug and having issues

--- server.js
+++ server.js
@@ -2,31 +2,45 @@
 
 var http = require('http');
 var express = require('express');
+//var pug = require('pug');
 
 const app = express();
 
-app.use( (request, response, next) =>{
-	console.log('In middleware 1');
+// browser -> node -> express -> M1 -> M2 -> M3 -> handler
+//         <-       <-         <-   <-    <-    <-
+
+app.set('view engine', 'pug');
+
+app.use(express.static('public'));
+
+app.use(function(request, response, next){
+	// console.log('In middleware 1');
 	response.write('HEADER ');
 	next();
-	console.log('Out of Middleware 1');
+	// console.log('Out of Middleware 1');
 });
 
-app.use((request, response, next) => {
-	console.log('In Middleware 2');
+app.use(function(request, response, next){
+	// console.log('In Middleware 2');
 	response.write('OTHER ')
 	next();
-	console.log('Out of Middleware 2');
+	// console.log('Out of Middleware 2');
 });
 
-app.get('/', (request, response) =>{
+app.get('/', function(request, response){
 	response.end('Hello World.');
-	console.log('In Handler');
+	// console.log('In Handler');
 });
+
+app.get('/home', function(request,response){
+	response.render('index', {title: "Title!!!!!!!"})
+});
+
+app.use(express.static('/public'));
 
 const server = new http.Server(app);
 
 const port = 3010;
 server.listen(port, () => {
-	console.log('Server started on port ' + port);
+	console.log(`Server started on port ${port}`);
 });
